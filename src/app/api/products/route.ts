6431855b210d44ded46359f315a0ac6506f4ddfd
Haidir Aditya <haidiraditya16@gmail.com>
2025-04-12 16:06:52 +0800

    next-viv-code-studio (App Router) -> (12) Connect Firebase DB

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,118 +1,121 @@
 // import { DataType } from "@/types/datatype";
 
+import { retrieveData, retrieveDataById } from "@/libs/firebase/service";
 import { NextRequest, NextResponse } from "next/server";
 
-const data: Array<{
-  id: string;
-  category: string;
-  image?: string;
-  name: string;
-  price: number;
-}> = [
-  {
-    id: "1cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Asus",
-    price: 12_000_000,
-  },
-  {
-    id: "2cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Acer",
-    price: 15_000_000,
-  },
-  {
-    id: "3cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Asus 2",
-    price: 12_000_000,
-  },
-  {
-    id: "4cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Acer 2",
-    price: 15_000_000,
-  },
-  {
-    id: "5cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Asus 3",
-    price: 12_000_000,
-  },
-  {
-    id: "6cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Acer 3",
-    price: 15_000_000,
-  },
-  {
-    id: "7cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Asus 4",
-    price: 12_000_000,
-  },
-  {
-    id: "8cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Acer 4",
-    price: 15_000_000,
-  },
-  {
-    id: "9cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Asus 5",
-    price: 12_000_000,
-  },
-  {
-    id: "10cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Acer 5",
-    price: 15_000_000,
-  },
-  {
-    id: "11cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Asus 6",
-    price: 12_000_000,
-  },
-  {
-    id: "12cuwiwaw",
-    category: "Elektronik",
-    image:
-      "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    name: "Laptop Acer 6",
-    price: 15_000_000,
-  },
-];
+// FAKE PRODUCTS
+// const data: Array<{
+//   id: string;
+//   category: string;
+//   image?: string;
+//   name: string;
+//   price: number;
+// }> = [
+//   {
+//     id: "1cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Asus",
+//     price: 12_000_000,
+//   },
+//   {
+//     id: "2cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Acer",
+//     price: 15_000_000,
+//   },
+//   {
+//     id: "3cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Asus 2",
+//     price: 12_000_000,
+//   },
+//   {
+//     id: "4cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Acer 2",
+//     price: 15_000_000,
+//   },
+//   {
+//     id: "5cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Asus 3",
+//     price: 12_000_000,
+//   },
+//   {
+//     id: "6cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Acer 3",
+//     price: 15_000_000,
+//   },
+//   {
+//     id: "7cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Asus 4",
+//     price: 12_000_000,
+//   },
+//   {
+//     id: "8cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Acer 4",
+//     price: 15_000_000,
+//   },
+//   {
+//     id: "9cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Asus 5",
+//     price: 12_000_000,
+//   },
+//   {
+//     id: "10cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Acer 5",
+//     price: 15_000_000,
+//   },
+//   {
+//     id: "11cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1636211990414-8edec17ba047?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Asus 6",
+//     price: 12_000_000,
+//   },
+//   {
+//     id: "12cuwiwaw",
+//     category: "Elektronik",
+//     image:
+//       "https://images.unsplash.com/photo-1693206578601-21cdc341d2c8?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     name: "Laptop Acer 6",
+//     price: 15_000_000,
+//   },
+// ];
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id");
 
   if (id) {
-    const product = data.find((item) => item.id === id);
+    // const product = data.find((item) => item.id === id);
+    const product = await retrieveDataById("products", id);
 
     if (product) {
       return NextResponse.json({
@@ -129,5 +132,7 @@ export async function GET(request: NextRequest) {
     }
   }
 
-  return NextResponse.json({ status: 200, message: "Success", data });
+  const products = await retrieveData("products");
+
+  return NextResponse.json({ status: 200, message: "Success", data: products });
 }
